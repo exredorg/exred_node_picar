acb403d2d8f89caa11a68c392caa3840e0a4c4ee
Zsolt <zsoltxyz@gmail.com>
2019-02-18 16:49:58 -0500

    took out tests, they would only work on a raspberry pi

--- test/exred_node_picar_test.exs
+++ test/exred_node_picar_test.exs
@@ -2,19 +2,19 @@ defmodule Exred.Node.PicarTest do
   use ExUnit.Case
   doctest Exred.Node.Picar
 
-  use Exred.NodeTest, module: Exred.Node.Picar
-
-  setup_all do
-    start_node()
-  end
-
-  test "PWM alive" do
-    assert Process.alive?(Process.whereis(Exred.Node.Picar.PWM))
-  end
-  test "FrontWheels alive" do
-    assert Process.alive?(Process.whereis(Exred.Node.Picar.FrontWheels))
-  end
-  test "RearWheels alive" do
-    assert Process.alive?(Process.whereis(Exred.Node.Picar.RearWheels))
-  end
+#   use Exred.NodeTest, module: Exred.Node.Picar
+# 
+#   setup_all do
+#     start_node()
+#   end
+# 
+#   test "PWM alive" do
+#     assert Process.alive?(Process.whereis(Exred.Node.Picar.PWM))
+#   end
+#   test "FrontWheels alive" do
+#     assert Process.alive?(Process.whereis(Exred.Node.Picar.FrontWheels))
+#   end
+#   test "RearWheels alive" do
+#     assert Process.alive?(Process.whereis(Exred.Node.Picar.RearWheels))
+#   end
 end
